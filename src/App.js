3cc1617e60a2353df888d7f0ec3db8d216c64fc2
Bishar <bishar995@gmail.com>
2025-06-21 09:25:41 +0300

    installed react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
 function App() {
-  return <div className=" text-red-600 text-2xl">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
